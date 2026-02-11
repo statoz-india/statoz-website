0ea83a9f9ab80b2e7a93ead4d9029baf4bc730fe
kshitiz-dayal <kshitizdr842@gmail.com>
2026-02-11 19:03:07 +0530

    changes

--- src/app/components/BlogsSection.tsx
+++ src/app/components/BlogsSection.tsx
@@ -10,7 +10,6 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import Slider, { type CustomArrowProps } from "react-slick";
-import { useEffect, useState } from "react";
 import { ImageWithFallback } from "./figma/ImageWithFallback";
 import { BLOG_POSTS, type BlogPost } from "../utils/blogData";
 import { useEffect, useState } from "react";
